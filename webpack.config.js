9951c14f89ea71728d5fe1d834d57aafbf221566
Mike Hearn <git@mikehearn.net>
2016-09-16 18:36:58 +0000

    Added linting and massively updated scss syntax

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     publicPath: '/wnyc_homework/dist/',
+    // publicPath: '/dist/',
     filename: 'bundle.js'
   },
   resolve: {
